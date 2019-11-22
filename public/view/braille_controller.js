d112194d3a1c9626d16b51bdb67dae85fbcdb81a
mohammad suliman <mohmad.s93@gmail.com>
2019-11-22 19:18:18 +0200

    Replace all ocurrences of double quotation marks with single quotation marks

--- public/view/braille_controller.js
+++ public/view/braille_controller.js
@@ -3,19 +3,19 @@ let brailleController = null;
 class BrailleController {
 
   constructor(parent) {
-    if (document.getElementById("braille_controller_text")) {
-      throw "Braille controller already created";
+    if (document.getElementById('braille_controller_text')) {
+      throw 'Braille controller already created';
     }
-    let textarea = document.createElement("textarea");
-    textarea.setAttribute("id", "braille_controller_text");
-    textarea.value = "⠊⠉⠑⢄⣀⡠⠊⠉⠑⢄⣀⡠⠊";
-    //textarea.className = "hidden";
-    textarea.addEventListener("keydown", this.eventCatcher);
-    textarea.addEventListener("keyup", this.eventCatcher);
-    textarea.addEventListener("keypress", this.eventCatcher);
-    textarea.addEventListener("click", this.eventCatcher);
-    textarea.addEventListener("mousedown", this.eventCatcher);
-    textarea.addEventListener("mouseup", this.eventCatcher);
+    let textarea = document.createElement('textarea');
+    textarea.setAttribute('id', 'braille_controller_text');
+    textarea.value = '⠊⠉⠑⢄⣀⡠⠊⠉⠑⢄⣀⡠⠊';
+    //textarea.className = 'hidden';
+    textarea.addEventListener('keydown', this.eventCatcher);
+    textarea.addEventListener('keyup', this.eventCatcher);
+    textarea.addEventListener('keypress', this.eventCatcher);
+    textarea.addEventListener('click', this.eventCatcher);
+    textarea.addEventListener('mousedown', this.eventCatcher);
+    textarea.addEventListener('mouseup', this.eventCatcher);
     parent.appendChild(textarea);
     textarea.focus();
 
@@ -24,25 +24,25 @@ class BrailleController {
     this.lastEvent = null;
     document.addEventListener('selectionchange',
       this.onRoutingKeyPress);
-    document.addEventListener("selectionchange", function () {
+    document.addEventListener('selectionchange', function () {
       brailleController.onRoutingKeyPress();
     });
   }
 
   eventCatcher(event) {
     // Second rooting key press in the same position is received as a mousedown event
-    if (event.target.id == "braille_controller_text") {
+    if (event.target.id == 'braille_controller_text') {
       // We ignore other events since we assume they belong to the same rooting key press
-      if (event.type == "mousedown") {
+      if (event.type == 'mousedown') {
         if (brailleController.lastEvent != null) {
           brailleController.listener(brailleController.lastEvent);
         } else {
-          console.error("BrailleController.lastEvent is null on supposedly second rooting key press");
+          console.error('BrailleController.lastEvent is null on supposedly second rooting key press');
         }
       }
     } else {
       debugger;
-      console.log("Event caught: " + event);
+      console.log('Event caught: ' + event);
     }
   }
 
@@ -60,8 +60,8 @@ class BrailleController {
     if (event.baseNode == null ||
       event.baseNode.parentElement == null ||
       event.baseNode.parentElement.offsetParent == null ||
-      event.baseNode.parentElement.offsetParent.id != "braille_controller_text") {
-      console.log("Selection not on braille_controller_text");
+      event.baseNode.parentElement.offsetParent.id != 'braille_controller_text') {
+      console.log('Selection not on braille_controller_text');
     }
 
     if (brailleController.listener == null) {
--- public/view/view.js
+++ public/view/view.js
@@ -9,11 +9,11 @@ let selectedCell = null;
 let timeOut = null;
 
 function routingKeyPressListener(event) {
-  console.log("routingKeyPressListener: cursorPosition=" + event.cursorPosition + " cursorPosition=" + event.character);
+  console.log('routingKeyPressListener: cursorPosition=' + event.cursorPosition + ' cursorPosition=' + event.character);
 }
 
 window.addEventListener('DOMContentLoaded', (event) => {
-  brailleController = new BrailleController(document.getElementById("container"));
+  brailleController = new BrailleController(document.getElementById('container'));
   brailleController.setRoutingKeyPressListener(routingKeyPressListener);
 });
 
@@ -24,65 +24,65 @@ function processData() {
 }
 
 function createGrid() {
-  let grid = document.createElement("div");
-  grid.setAttribute("role", "grid");
-  grid.setAttribute("id", "grid");
-  grid.setAttribute("aria-readonly", "true");
-  grid.style.width = "100%";
-  grid.style.height = "70%";
-  grid.className = "table";
-  let input = getDataFromURL("data");
-  let lines = input.split("\n");
+  let grid = document.createElement('div');
+  grid.setAttribute('role', 'grid');
+  grid.setAttribute('id', 'grid');
+  grid.setAttribute('aria-readonly', 'true');
+  grid.style.width = '100%';
+  grid.style.height = '70%';
+  grid.className = 'table';
+  let input = getDataFromURL('data');
+  let lines = input.split('\n');
   let line;
   let rowIndex = 0;
   for (line of lines) {
-    let gridRow = document.createElement("div");
-    gridRow.setAttribute("role", "row");
-    gridRow.className = "row";
+    let gridRow = document.createElement('div');
+    gridRow.setAttribute('role', 'row');
+    gridRow.className = 'row';
     let columnIndex = 0;
-    let values = line.split("\t");
+    let values = line.split('\t');
     let value;
     for (value of values) {
-      let gridCell = document.createElement("div");
-      gridCell.setAttribute("role", "gridcell");
-      gridCell.className = "cell";
+      let gridCell = document.createElement('div');
+      gridCell.setAttribute('role', 'gridcell');
+      gridCell.className = 'cell';
       gridCell.appendChild(document.createTextNode(value));
-      gridCell.setAttribute("aria-readonly", "true");
-      gridCell.setAttribute("row", rowIndex);
-      gridCell.setAttribute("col", columnIndex);
+      gridCell.setAttribute('aria-readonly', 'true');
+      gridCell.setAttribute('row', rowIndex);
+      gridCell.setAttribute('col', columnIndex);
       gridRow.appendChild(gridCell);
       columnIndex++;
     }
     grid.appendChild(gridRow);
     rowIndex++;
   }
-  let container = document.getElementById("container");
+  let container = document.getElementById('container');
   container.appendChild(grid);
 }
 
 function addOnClickAndOnTouchSoundToGrid() {
   let element;
-  for (element of document.querySelectorAll("div[role='gridcell']")) {
-    element.addEventListener("click", startSoundPlayback);
-    element.addEventListener("touchstart", startSoundPlayback);
-    element.addEventListener("touchmove", onCellChange);
-    element.addEventListener("touchleave", stopSoundPlayback);
-    element.addEventListener("touchcancel", stopSoundPlayback);
-    element.addEventListener("focus", startSoundPlayback);
+  for (element of document.querySelectorAll('div[role="gridcell"]')) {
+    element.addEventListener('click', startSoundPlayback);
+    element.addEventListener('touchstart', startSoundPlayback);
+    element.addEventListener('touchmove', onCellChange);
+    element.addEventListener('touchleave', stopSoundPlayback);
+    element.addEventListener('touchcancel', stopSoundPlayback);
+    element.addEventListener('focus', startSoundPlayback);
   }
 }
 
 function addNavigationToGrid() {
   let firstElement = true;
   let gridCell;
-  for (gridCell of document.querySelectorAll("div[role='gridcell']")) {
+  for (gridCell of document.querySelectorAll('div[role="gridcell"]')) {
     if (firstElement == true) {
-      gridCell.setAttribute("tabindex", "0");
+      gridCell.setAttribute('tabindex', '0');
       firstElement = false;
     } else {
-      gridCell.setAttribute("tabindex", "-1");
+      gridCell.setAttribute('tabindex', '-1');
     }
-    gridCell.addEventListener("keydown", navigateGrid);
+    gridCell.addEventListener('keydown', navigateGrid);
   }
 }
 
@@ -91,28 +91,28 @@ function navigateGrid(event) {
   let currentCell = event.currentTarget;
   let newFocusedCell = null;
   switch (keyName) {
-    case "ArrowDown":
+    case 'ArrowDown':
       if (currentCell.parentNode.nextSibling != null) {
-        let index = currentCell.getAttribute("col");
+        let index = currentCell.getAttribute('col');
         newFocusedCell = currentCell.parentNode.nextSibling.childNodes[index];
       }
       break;
-    case "ArrowUp":
+    case 'ArrowUp':
       if (currentCell.parentNode.previousSibling != null) {
-        let index = currentCell.getAttribute("col");
+        let index = currentCell.getAttribute('col');
         newFocusedCell = currentCell.parentNode.previousSibling.childNodes[index];
       }
       break;
-    case "ArrowLeft":
+    case 'ArrowLeft':
       newFocusedCell = currentCell.previousSibling;
       break;
-    case "ArrowRight":
+    case 'ArrowRight':
       newFocusedCell = currentCell.nextSibling;
       break;
-    case "Home":
+    case 'Home':
       newFocusedCell = currentCell.parentNode.firstChild;
       break;
-    case "End":
+    case 'End':
       newFocusedCell = currentCell.parentNode.lastChild;
       break;
     // TODO: add PageUp/Down keys
@@ -132,11 +132,11 @@ function navigateGrid(event) {
 * Cells in a 2X2 grid will be positioned between -1.5 and 1.5 in both x and y.
 */
 function get2DCoordinates(currentCell) {
-  let grid = document.getElementById("grid");
+  let grid = document.getElementById('grid');
   let columnCount = grid.firstChild.childNodes.length;
   // The col attribute is zero based indexe
   // For example, the value of  col attribute for a cell found in the third column is 2
-  let columnNumber = currentCell.getAttribute("col");
+  let columnNumber = currentCell.getAttribute('col');
   let xCoordinate = columnNumber - Math.floor(columnCount / 2);
   // Align xCoordinate to be symmetric with respect to y-axis
   if (columnCount % 2 == 0) {
@@ -144,7 +144,7 @@ function get2DCoordinates(currentCell) {
   }
   let rowCount = grid.childNodes.length;
   // The same applies for row attribute as col one
-  let rowNumber = currentCell.getAttribute("row");
+  let rowNumber = currentCell.getAttribute('row');
   let yCoordinate = rowNumber - Math.floor(rowCount / 2);
   // Align yCoordinate similar to xCoordinate:
   if (rowCount % 2 == 0) {
@@ -166,7 +166,7 @@ function get2DCoordinates(currentCell) {
 *  and the number of cells in each column is equal also
 */
 function getMaxDistancePossible() {
-  let grid = document.getElementById("grid");
+  let grid = document.getElementById('grid');
   // The cell with max coordinates is naturaly found in the upper right corner of the grid
   // so get it
   let cellWithMaxCoordinates = grid.firstChild.lastChild;
@@ -181,8 +181,8 @@ function getMaxDistancePossible() {
 
 function createAndSetPanner(currentCell) {
   let panner = audioContext.createPanner();
-  panner.panningModel = "HRTF";
-  panner.distanceModel = "linear";
+  panner.panningModel = 'HRTF';
+  panner.distanceModel = 'linear';
   panner.refDistance = 0;
   panner.rolloffFactor = panner.maxDistance / (getMaxDistancePossible() * 2);
   let coordinates = get2DCoordinates(currentCell);
@@ -195,8 +195,8 @@ function createAndSetOscillator(currentCell) {
   let selectedValue = currentCell.firstChild.data;
   const MAX_FREQUENCY = 1000;
   const MIN_FREQUENCY = 100;
-  let minValue = getDataFromURL("minValue");
-  let maxValue = getDataFromURL("maxValue");
+  let minValue = getDataFromURL('minValue');
+  let maxValue = getDataFromURL('maxValue');
   selectedValue = parseFloat(selectedValue);
   minValue = parseFloat(minValue);
   maxValue = parseFloat(maxValue);
@@ -219,10 +219,10 @@ function startSoundPlayback(event) {
 }
 
 function playSound(event) {
-  if (audioContext.state == "suspended") {
+  if (audioContext.state == 'suspended') {
     audioContext.resume();
   }
-  if (getDataFromURL("instrumentType") == "synthesizer") {
+  if (getDataFromURL('instrumentType') == 'synthesizer') {
     playSoundWithOscillator();
   } else {
     playSoundFromAudioFile();
@@ -245,8 +245,8 @@ function playSoundWithOscillator() {
 function playSoundFromAudioFile() {
   let fileName = getFileToPlay(selectedCell);
   let request = new XMLHttpRequest();
-  request.open("get", fileName, true);
-  request.responseType = "arraybuffer";
+  request.open('get', fileName, true);
+  request.responseType = 'arraybuffer';
   request.onload = function () {
     let data = request.response;
     audioContext.decodeAudioData(data, playAudioFile);
@@ -264,8 +264,8 @@ function playAudioFile(buffer) {
 }
 
 function getFileToPlay(currentCell) {
-  let minValue = getDataFromURL("minValue");
-  let maxValue = getDataFromURL("maxValue");
+  let minValue = getDataFromURL('minValue');
+  let maxValue = getDataFromURL('maxValue');
   let selectedValue = currentCell.firstChild.data;
   minValue = parseFloat(minValue);
   maxValue = parseFloat(maxValue);
@@ -276,9 +276,9 @@ function getFileToPlay(currentCell) {
   if (trackNumber == 0) {
     trackNumber++;
   }
-  let instrumentType = getDataFromURL("instrumentType");
-  let fileName = "/assets/" + instrumentType;
-  fileName += "/track" + trackNumber + ".mp3";
+  let instrumentType = getDataFromURL('instrumentType');
+  let fileName = '/assets/' + instrumentType;
+  fileName += '/track' + trackNumber + ''.mp3';
   return fileName;
 }
 
@@ -308,7 +308,7 @@ function onCellChange(event) {
   if (elementUnderTouch == selectedCell) {
     return;
   }
-  if (elementUnderTouch == null || elementUnderTouch.getAttribute("role") != "gridcell") {
+  if (elementUnderTouch == null || elementUnderTouch.getAttribute('role') != 'gridcell') {
     return;
   }
   selectedCell = elementUnderTouch;
@@ -321,7 +321,7 @@ function getDataFromURL(variableName) {
   let url = new URL(window.location.href);
   let params = url.searchParams;
   if (params.has(variableName) == false) {
-    return "";
+    return '';
   }
   return params.get(variableName);
 }
