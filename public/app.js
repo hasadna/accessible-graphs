84722a8a9e98503e0516b3312969da347972fbb0
S0AndS0 <strangerthanbland@gmail.com>
2020-04-09 21:58:27 -0700

    :fire: removes duplicate functions from `app.{ts,js,js.map}`

--- public/app.js
+++ public/app.js
@@ -12,50 +12,6 @@ function initializeAppScript() {
     }
 }
 /**
- * Sets `window.location.href` with search paramater data to be parsed by `view.js`
- */
-function updateURL() {
-    const input = encodeURIComponent(document.getElementById('dataInput').value);
-    const minValue = document.getElementById('minValue').value;
-    const maxValue = document.getElementById('maxValue').value;
-    const instrumentType = document.getElementById('instrumentType').value;
-    const ttsVoiceIndex = document.getElementById('ttsVoice').selectedIndex;
-    let newUrl = '';
-    // Check if we are running on a localhost
-    if (['localhost', '127.0.0.1', ''].includes(location.hostname)) {
-        newUrl = window.location.pathname.split('/').slice(0, -1).join('/');
-    }
-    else {
-        newUrl = new URL(window.location.href).origin;
-    }
-    newUrl += '/view/index.html?';
-    newUrl += `data=${input}`;
-    newUrl += `&minValue=${minValue}`;
-    newUrl += `&maxValue=${maxValue}`;
-    newUrl += `&instrumentType=${instrumentType}`;
-    newUrl += `&ttsIndex=${ttsVoiceIndex}`;
-    window.location.href = newUrl;
-}
-/**
- * Callback for radio input `autoOption` and `manualOption` elements
- */
-function onRadioChange(radio) {
-    const minValuePicker = document.getElementById('minValue');
-    const maxValuePicker = document.getElementById('maxValue');
-    if (radio.value === 'auto') {
-        minValuePicker.disabled = true;
-        maxValuePicker.disabled = true;
-    }
-    else {
-        minValuePicker.disabled = false;
-        maxValuePicker.disabled = false;
-        /**
-         * @note: `updateButton` element ID does **not** seem to exist within HTML document
-         */
-        // (<HTMLInputElement> document.getElementById('updateButton')).disabled = false;
-    }
-}
-/**
  *
  * @param {string} input - String with `\r\n` or `\n` line separators, and either spaces or tabs separating items
  */
@@ -79,38 +35,5 @@ function getMinMaxValuesFrom(input) {
         });
     });
     return { maxValue, minValue };
-}
-/**
- * Sets values of min and max HTML IDs from values within values HTML ID
- * @param {string} values_html_id - HTML ID to read `.value` from
- * @param {string} min_html_id - HTML ID to set `.value` to min value
- * @param {string} max_html_id - HTML ID to set `.value` to max value
- */
-function setMinAndMaxValuesFrom(values_html_id = 'dataInput', min_html_id = 'minValue', max_html_id = 'maxValue') {
-    if (document.getElementById('autoOption').checked === false) {
-        return;
-    }
-    const input = document.getElementById(values_html_id).value;
-    const { maxValue, minValue } = getMinMaxValuesFrom(input);
-    document.getElementById(min_html_id).value = maxValue.toString();
-    document.getElementById(max_html_id).value = minValue.toString();
-}
-/**
- * Populates `select` element ID "ttsVoice"
- */
-function populateTtsList() {
-    const ttsVoiceSelect = document.getElementById('ttsVoice');
-    ttsVoiceSelect.innerHTML = '';
-    const voices = window.speechSynthesis.getVoices();
-    voices.forEach((voice) => {
-        const option = document.createElement('option');
-        let optionValueAndText = `${voice.name}(${voice.lang})`;
-        if (voice.default === true) {
-            optionValueAndText += ' -- DEFAULT';
-        }
-        option.value = optionValueAndText;
-        option.innerText = optionValueAndText;
-        ttsVoiceSelect.appendChild(option);
-    });
 }
 //# sourceMappingURL=app.js.map
--- ts/app.ts
+++ ts/app.ts
@@ -12,52 +12,6 @@ function initializeAppScript() {
   }
 }
 
-/**
- * Sets `window.location.href` with search paramater data to be parsed by `view.js`
- */
-function updateURL() {
-  const input: string = encodeURIComponent((<HTMLInputElement> document.getElementById('dataInput')).value);
-  const minValue: string = (<HTMLInputElement> document.getElementById('minValue')).value;
-  const maxValue: string = (<HTMLInputElement> document.getElementById('maxValue')).value;
-  const instrumentType: string = (<HTMLInputElement> document.getElementById('instrumentType')).value;
-  const ttsVoiceIndex: number = (<HTMLSelectElement> document.getElementById('ttsVoice')).selectedIndex;
-
-  let newUrl: string = '';
-  // Check if we are running on a localhost
-  if (['localhost', '127.0.0.1', ''].includes(location.hostname)) {
-    newUrl = window.location.pathname.split('/').slice(0, -1).join('/');
-  } else {
-    newUrl = new URL(window.location.href).origin;
-  }
-  newUrl += '/view/index.html?';
-  newUrl += `data=${input}`;
-  newUrl += `&minValue=${minValue}`;
-  newUrl += `&maxValue=${maxValue}`;
-  newUrl += `&instrumentType=${instrumentType}`;
-  newUrl += `&ttsIndex=${ttsVoiceIndex}`;
-  window.location.href = newUrl;
-}
-
-
-/**
- * Callback for radio input `autoOption` and `manualOption` elements
- */
-function onRadioChange(radio: HTMLInputElement) {
-  const minValuePicker: HTMLInputElement = (<HTMLInputElement> document.getElementById('minValue'));
-  const maxValuePicker: HTMLInputElement = (<HTMLInputElement> document.getElementById('maxValue'));
-  if (radio.value === 'auto') {
-    minValuePicker.disabled = true;
-    maxValuePicker.disabled = true;
-  } else {
-    minValuePicker.disabled = false;
-    maxValuePicker.disabled = false;
-    /**
-     * @note: `updateButton` element ID does **not** seem to exist within HTML document
-     */
-    // (<HTMLInputElement> document.getElementById('updateButton')).disabled = false;
-  }
-}
-
 
 /**
  *
@@ -83,50 +37,4 @@ function getMinMaxValuesFrom(input: string) {
     });
   });
   return {maxValue, minValue};
-}
-
-
-/**
- * Sets values of min and max HTML IDs from values within values HTML ID
- * @param {string} values_html_id - HTML ID to read `.value` from
- * @param {string} min_html_id - HTML ID to set `.value` to min value
- * @param {string} max_html_id - HTML ID to set `.value` to max value
- */
-function setMinAndMaxValuesFrom(
-  values_html_id: string = 'dataInput',
-  min_html_id: string = 'minValue',
-  max_html_id: string = 'maxValue'
-) {
-  if ((<HTMLInputElement> document.getElementById('autoOption')).checked === false) {
-    return;
-  }
-
-  const input = (<HTMLInputElement> document.getElementById(values_html_id)).value;
-  const {maxValue, minValue} = getMinMaxValuesFrom(input);
-
-  (<HTMLInputElement> document.getElementById(min_html_id)).value = maxValue.toString();
-  (<HTMLInputElement> document.getElementById(max_html_id)).value = minValue.toString();
-}
-
-
-/**
- * Populates `select` element ID "ttsVoice"
- */
-function populateTtsList() {
-  const ttsVoiceSelect: HTMLElement = document.getElementById('ttsVoice');
-  ttsVoiceSelect.innerHTML = '';
-
-  const voices: Array<SpeechSynthesisVoice> = window.speechSynthesis.getVoices();
-  voices.forEach((voice) => {
-    const option = document.createElement('option');
-    let optionValueAndText: string = `${voice.name}(${voice.lang})`;
-
-    if (voice.default === true) {
-      optionValueAndText += ' -- DEFAULT';
-    }
-
-    option.value = optionValueAndText;
-    option.innerText = optionValueAndText;
-    ttsVoiceSelect.appendChild(option);
-  });
 }
