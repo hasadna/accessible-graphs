9e95279067568fb41b86624e4e3dcee7b80750d3
Mohammad Suliman <mohmad.s93@gmail.com>
2020-04-12 10:43:55 +0300

    Some cosmetic edits

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,32 +1,25 @@
 const changeLanguage = (language) => {
-    $('#language-popup').modal('toggle');
-    console.log(language);
-    setLanguage(language);
-    if(lang ==='he'){
-        document.body.setAttribute('dir', 'rtl');
-        if(!document.getElementById('aboutP1').classList.contains('text-right')) {
-            document.getElementsByTagName('H3')[0].classList.toggle('text-right');
-            document.getElementById('aboutP1').classList.toggle('text-right');
-            document.getElementById('aboutP2').classList.toggle('text-right');
-            document.getElementById('aboutP3').classList.toggle('text-right');
-        }
+  $('#language-popup').modal('toggle');
+  console.log(language);
+  setLanguage(language);
+  if (lang === 'he') {
+    document.body.setAttribute('dir', 'rtl');
+    if (!document.getElementById('aboutP1').classList.contains('text-right')) {
+      document.getElementsByTagName('H3')[0].classList.toggle('text-right');
+      document.getElementById('aboutP1').classList.toggle('text-right');
+      document.getElementById('aboutP2').classList.toggle('text-right');
+      document.getElementById('aboutP3').classList.toggle('text-right');
     }
-    else{
-        if(lang==='en'){
-            document.body.setAttribute('dir', 'ltr');
-            if(document.getElementById('aboutP1').classList.contains('text-right')){
-                document.getElementsByTagName('H3')[0].classList.toggle('text-right');
-                document.getElementById('aboutP1').classList.toggle('text-right');
-                document.getElementById('aboutP2').classList.toggle('text-right');
-                document.getElementById('aboutP3').classList.toggle('text-right');
-            }
-        }
+  }
+  else {
+    if (lang === 'en') {
+      document.body.setAttribute('dir', 'ltr');
+      if (document.getElementById('aboutP1').classList.contains('text-right')) {
+        document.getElementsByTagName('H3')[0].classList.toggle('text-right');
+        document.getElementById('aboutP1').classList.toggle('text-right');
+        document.getElementById('aboutP2').classList.toggle('text-right');
+        document.getElementById('aboutP3').classList.toggle('text-right');
+      }
     }
+  }
 };
-
-
-
-
-
-
-
--- public/languages/i18n.js
+++ public/languages/i18n.js
@@ -4,57 +4,57 @@ let langDirection;
 
 
 const getLanguage = () => {
-    let lang = getQueryParam('lang');
-    if (lang) {
-        return lang.toLowerCase();
-    }
-    lang = localStorage.getItem('language');
-    if (lang) {
-        return lang.toLowerCase();
-    }
-    return 'en';
+  let lang = getQueryParam('lang');
+  if (lang) {
+    return lang.toLowerCase();
+  }
+  lang = localStorage.getItem('language');
+  if (lang) {
+    return lang.toLowerCase();
+  }
+  return 'en';
 };
 
 // Save language in local storage if not already there, update HTML
 const setLanguage = (selectedLang) => {
-    lang = selectedLang;
-    localStorage.setItem('language', lang);
-    setQueryParam('lang', lang);
-    langDirection = langs[lang].direction;
-    setTranslationInHTML();
-    document.title = i18n('title');
+  lang = selectedLang;
+  localStorage.setItem('language', lang);
+  setQueryParam('lang', lang);
+  langDirection = langs[lang].direction;
+  setTranslationInHTML();
+  document.title = i18n('title');
 };
 
 const setTranslationInHTML = () => {
-    // For keys that match element IDs:
-    for (let key in langs[lang]) {
-        setTranslationByID(key, key);
-    }
-    setTranslationByID('select-language-header', 'selectLanguage');
-    $(document).ready(function() {
-        $("html").attr("lang", lang); //'language' value is retrieved from a cookie
-    });
+  // For keys that match element IDs:
+  for (let key in langs[lang]) {
+    setTranslationByID(key, key);
+  }
+  setTranslationByID('select-language-header', 'selectLanguage');
+  $(document).ready(function () {
+    $('html').attr('lang', lang); //'language' value is retrieved from a cookie
+  });
 };
 
 const setTranslationByID = (id, langKey) => {
-    const el = document.getElementById(id);
-    if (el) {
-        el.innerText = i18n(langKey);
-    }
+  const el = document.getElementById(id);
+  if (el) {
+    el.innerText = i18n(langKey);
+  }
 };
 
 const i18n = langKey => {
-    if (langs[lang].hasOwnProperty(langKey)) {
-        return langs[lang][langKey];
-    } else {
-        console.log(langKey);
-        return langs['en'][langKey];
-    }
+  if (langs[lang].hasOwnProperty(langKey)) {
+    return langs[lang][langKey];
+  } else {
+    console.log(langKey);
+    return langs['en'][langKey];
+  }
 };
 
 const initLanguage = () => {
-    // Get language from url or local storage
-    lang = getLanguage();
-    // Set it, and update everything needed
-    setLanguage(lang);
+  // Get language from url or local storage
+  lang = getLanguage();
+  // Set it, and update everything needed
+  setLanguage(lang);
 };
