0df4031f9174192c6dc1f409135fa327a0c7c120
Mohammad Suliman <mohmad.s93@gmail.com>
2020-04-16 17:41:27 +0300

    Make links that open in a new tab more accessible

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -93,3 +93,23 @@ $('.language-click').click(function () {
   $('#language-popup').modal('toggle');
 
 });
+
+// Links that opens in a new tabs accessibility and security adjusments
+function addNoOpener(link) {
+  let linkTypes = (link.getAttribute('rel') || '').split(' ');
+  if (!linkTypes.includes('noopener')) {
+      linkTypes.push('noopener');
+  }
+  link.setAttribute('rel', linkTypes.join(' ').trim());
+}
+
+function addNewTabMessage(link) {
+  if (!link.querySelector('.screen-reader-only')) {
+      link.insertAdjacentHTML('beforeend', '<span class="screen-reader-only">(opens in a new tab)</span>');
+  }
+}
+
+document.querySelectorAll('a[target="_blank"]').forEach(link => {
+  addNoOpener(link);
+  addNewTabMessage(link);
+});
