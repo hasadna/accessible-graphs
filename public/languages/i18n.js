e510cd99370b1a742ee5377b05329972948d84d2
Tomer Bitan <Tomerbitan1993@gmail.com>
2020-04-27 18:40:40 +0300

    fix i18n code

--- public/languages/i18n.js
+++ public/languages/i18n.js
@@ -59,29 +59,10 @@ const initLanguage = () => {
   setLanguage(lang);
 };
 
+
 //Save on the design between language
 const changeLanguage = (language) => {
   $('#language-popup').modal('toggle');
   console.log(language);
   setLanguage(language);
-  if (lang === 'he') {
-    document.body.setAttribute('dir', 'rtl');
-    if (!document.getElementById('aboutP1').classList.contains('text-right')) {
-      document.getElementsByTagName('H3')[0].classList.toggle('text-right');
-      document.getElementById('aboutP1').classList.toggle('text-right');
-      document.getElementById('aboutP2').classList.toggle('text-right');
-      document.getElementById('aboutP3').classList.toggle('text-right');
-    }
-  }
-  else {
-    if (lang === 'en') {
-      document.body.setAttribute('dir', 'ltr');
-      if (document.getElementById('aboutP1').classList.contains('text-right')) {
-        document.getElementsByTagName('H3')[0].classList.toggle('text-right');
-        document.getElementById('aboutP1').classList.toggle('text-right');
-        document.getElementById('aboutP2').classList.toggle('text-right');
-        document.getElementById('aboutP3').classList.toggle('text-right');
-      }
-    }
-  }
 };
