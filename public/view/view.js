34a4bd6ff09bce7338b9b5ffeb5b58fc244dc8d0
Mohammad Suliman <mohmad.s93@gmail.com>
2020-05-19 02:46:13 +0300

    Hide the grid

--- public/view/view.js
+++ public/view/view.js
@@ -46,6 +46,7 @@ function createGrid() {
     grid.setAttribute('role', 'grid');
     grid.setAttribute('id', 'grid');
     grid.setAttribute('aria-readonly', 'true');
+    grid.setAttribute('aria-hidden', 'true');
     grid.style.width = '100%';
     grid.style.height = '90%';
     grid.setAttribute('class', 'table');
@@ -91,12 +92,6 @@ function onClick(event) {
 }
 function addNavigationToGrid() {
     document.querySelectorAll('[role="gridcell"]').forEach((gridCell, index) => {
-        if (index === 0) {
-            gridCell.setAttribute('tabindex', '0');
-        }
-        else {
-            gridCell.setAttribute('tabindex', '-1');
-        }
         gridCell.addEventListener('keydown', navigateGrid);
     });
 }
