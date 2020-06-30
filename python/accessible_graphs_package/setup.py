fa9bb6ac7cf351be5ffa479cae3f46b83b264cfb
Mohammad Suliman <mohmad.s93@gmail.com>
2020-06-30 11:20:33 +0300

    Update github link

--- python/accessible_graphs_package/setup.py
+++ python/accessible_graphs_package/setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="accessible_graphs", 
-    version="0.0.3",
+    version="0.0.3.1",
     author="Mohammad Suliman",
     author_email="mohmad.s93@gmail.com",
     description="A package to make specific types of graphs accessible to low vision and blind users",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/hasadna/sensory-interface",
+    url="https://github.com/hasadna/accessible-graphs",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
