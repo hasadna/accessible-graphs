333f1b28a7e90f8be39fdfd330b5c8bd3b9f6540
mohammad-suliman <mohmad.s93@gmail.com>
2020-08-03 17:06:35 +0300

    complete the timeline implementation (#167)

--- public/crm/app.js
+++ public/crm/app.js
@@ -1,10 +1,12 @@
 let app;
 let db;
 let state;
-let currentIdUnderEditing = null;
+let currentContactId = null;
+let currentContactName = null;
 // Possible viewType values:
 const ENTITY_TABLE = 'ENTITY_TABLE';
 const EDIT_ENTITY = 'EDIT_ENTITY';
+const TIMELINE_ENTITY = 'TIMELINE_ENTITY';
 const NEW_CONTACT_FORM = 'NEW_ConTACT_FORM';
 function run() {
   let prodConfig = {
@@ -57,9 +59,6 @@ function toggleSignIn() {
     // [START createprovider]
     var provider = new firebase.auth.GoogleAuthProvider();
     // [END createprovider]
-    // [START addscopes]
-    provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
-    // [END addscopes]
     // [START signin]
     firebase.auth().signInWithPopup(provider).then(function (result) {
     }).catch(function (error) {
@@ -108,8 +107,8 @@ function render(state) {
     showDiv('add_contact_button_container');
     showDiv('backup_button_container');
     showDiv('sign_in_container');
-    hideDiv('edit_entity');
     hideDiv('add_contact_form');
+    hideDiv('timeline_entity');
     document.getElementById('newContact').focus();
   } else if (state.viewType === EDIT_ENTITY) {
     // Clears previous single-entity view, if applicable.
@@ -119,6 +118,7 @@ function render(state) {
     hideDiv('entity_table');
     hideDiv('add_contact_form');
     hideDiv('add_contact_button_container');
+    hideDiv('timeline_entity');
     showDiv('edit_entity');
   } else if (state.viewType === NEW_CONTACT_FORM) {
     hideDiv('edit_entity');
@@ -126,7 +126,17 @@ function render(state) {
     hideDiv('add_contact_button_container');
     hideDiv('sign_in_container');
     hideDiv('backup_button_container');
+    hideDiv('timeline_entity');
     showDiv('add_contact_form');
+  } else if (state.viewType === TIMELINE_ENTITY) {
+    hideDiv('edit_entity');
+    hideDiv('entity_table');
+    hideDiv('add_contact_button_container');
+    hideDiv('sign_in_container');
+    hideDiv('backup_button_container');
+    showDiv('timeline_entity');
+    document.getElementById('timeline_entity').innerHTML = '';
+    showContactTimeline(state.entityId);
   }
 }
 
@@ -145,6 +155,7 @@ function showNewContactForm() {
   removeListenerFromForm(updateContact);
   showDiv('new_contact_buttons');
   hideDiv('update_contact_buttons');
+  document.getElementById('contactFormHeading').innerHTML = 'Add new contact';
 }
 
 async function addContact(event) {
@@ -152,6 +163,7 @@ async function addContact(event) {
     event.preventDefault();
   }
   let doc = getDocFromForm();
+  doc['Timeline'] = '[]';
   try {
     let docRef = await db.collection("contacts").add(doc);
     console.log("Document written with ID: ", docRef.id);
@@ -201,15 +213,32 @@ function addTableRow(keys, value_fn, is_header) {
   return tr;
 }
 
-const fields = ["Name", "Next Action", "Do Date", "Status", "Comment", "Email", "Mobile Phone", "Office Phone", "Address", "Description", "Website", "Type", "Edit"];
+const fields = ["Name", "Next Action", "Do Date", "Status", "Timeline", "Email", "Mobile Phone", "Office Phone", "Address", "Description", "Website", "Type", "Edit"];
 const timestampFields = ["Do Date"];
 
 function getCellElement(field, entity) {
   if (field === 'Edit') {
-    a = document.createElement('a');
-    a.appendChild(document.createTextNode(`Edit`));
+    let a = document.createElement('a');
+    a.appendChild(document.createTextNode(`Edit contact card for ${currentContactName}`));
     a.href = `javascript:editSingleEntity('${entity.id}')`;
     return a;
+  } else if (field === 'Timeline') {
+    let a = document.createElement('a');
+    a.appendChild(document.createTextNode(`Show Timeline for ${currentContactName}`));
+    a.href = `javascript:showTimeline('${entity.id}')`;
+    return a;
+  } else if (field === 'Email') {
+    let a = document.createElement('a');
+    a.appendChild(document.createTextNode(entity.get('Email')));
+    a.href = `mailto:${entity.get('Email')}`;
+    return a;
+  } else if (field === 'Website') {
+    let website = entity.get('Website');
+    let a = document.createElement('a');
+    let textNode = document.createTextNode(website);
+    a.appendChild(textNode);
+    a.href = website;
+    return website !== '' ? a : textNode;
   } else if (timestampFields.includes(field)) {
     return document.createTextNode(entity.get(field).toDate().toDateString());
   } else {
@@ -229,6 +258,7 @@ function renderEntityTable() {
   tbody.appendChild(addTableRow(fields, (name) => document.createTextNode(name), true));
   db.collection("contacts").get().then((querySnapshot) => {
     querySnapshot.forEach((entity) => {
+      currentContactName = entity.get('Name');
       tbody.appendChild(addTableRow(fields, (field) => getCellElement(field, entity), false));
     });
   });
@@ -248,7 +278,7 @@ function editSingleEntity(id) {
   state = { 'viewType': EDIT_ENTITY, 'entityId': id };
   render(state)
   history.pushState(state, null, '');
-  currentIdUnderEditing = id;
+  currentContactId = id;
 }
 
 function renderSingleEntityEditor(id) {
@@ -265,8 +295,207 @@ function renderSingleEntityEditor(id) {
   });
 }
 
+function showTimeline(id) {
+  document.getElementById('timeline_entity').innerHTML = '';
+  state = { 'viewType': TIMELINE_ENTITY, 'entityId': id };
+  render(state);
+  history.pushState(state, null, '');
+  currentContactId = id;
+}
+
+async function getTimelineData(id) {
+  try {
+    let doc = await db.collection("contacts").doc(id).get();
+    if (doc.exists) {
+      return doc;
+    } else {
+      // doc.data() will be undefined in this case
+      console.log(`No document with ID ${id}`);
+    }
+  } catch (error) {
+    console.log(`Error getting document with ID ${id}:`, error);
+  }
+}
+
+async function showContactTimeline(id) {
+  let doc = await getTimelineData(id);
+  let timelineContainer = document.getElementById('timeline_entity');
+  let timeLineHeading = document.createElement('h1');
+  timeLineHeading.innerHTML = `Showing timeline for ${doc.get('Name')}`;
+  timelineContainer.appendChild(timeLineHeading);
+  let timelineInfo = doc.get('Timeline');
+  let items = JSON.parse(timelineInfo);
+  items.forEach((item) => {
+    let dateDiv = document.createElement('div');
+    dateDiv.id = item.date;
+    timelineContainer.appendChild(dateDiv);
+    let dateHeading = document.createElement('h2');
+    dateHeading.innerHTML = item.date;
+    dateDiv.appendChild(dateHeading);
+    item.elements.forEach((element) => {
+      let time = document.createElement('h3');
+      time.innerHTML = element.time;
+      dateDiv.appendChild(time);
+      let body = document.createElement('div');
+      let converter = new showdown.Converter();
+      body.innerHTML = converter.makeHtml(element.body);
+      body.setAttribute('data-markdown', element.body);
+      body.setAttribute('dir', 'auto');
+      dateDiv.appendChild(body);
+      let editButton = document.createElement('button');
+      editButton.id = 'editTimelineItem';
+      editButton.innerHTML = 'Edit item';
+      editButton.addEventListener('click', editTimelineItem);
+      dateDiv.appendChild(editButton);
+    });
+  });
+  let newItemHeading = document.createElement('h2');
+  newItemHeading.innerHTML = 'Add new item to timeline';
+  timelineContainer.appendChild(newItemHeading);
+  let newItemInput = document.createElement('textarea');
+  newItemInput.id = 'newItemInput';
+  timelineContainer.appendChild(newItemInput);
+  let addButton = document.createElement('button');
+  addButton.innerHTML = 'Add item to timeline';
+  addButton.addEventListener('click', addNewItem);
+  timelineContainer.appendChild(addButton);
+  let doneButton = document.createElement('button');
+  doneButton.innerHTML = 'Done';
+  doneButton.addEventListener('click', backToHome);
+  timelineContainer.appendChild(doneButton);
+}
+
+async function addNewItem(event) {
+  let docRef = db.collection("contacts").doc(currentContactId);
+  try {
+    let doc = await docRef.get();
+    if (doc.exists) {
+      let newTimelineElement = { "time": getCurrentTime(), "body": document.getElementById('newItemInput').value };
+      let currentDate = new Date().toDateString();
+      let items = addItemToDoc(doc, newTimelineElement, currentDate);
+      updateDoc(items);
+    } else {
+      // doc.data() will be undefined in this case
+      console.log(`No document with ID ${id}`);
+    }
+  } catch (error) {
+    console.log(`Error getting document with ID ${id}:`, error);
+  }
+}
+
+function backToHome(event) {
+  state = { 'viewType': 'ENTITY_TABLE' };
+  history.replaceState(state, null, '');
+  render(state);
+}
+
+function addItemToDoc(doc, newTimelineElement, date) {
+  let timelineInfo = doc.get('Timeline');
+  let items = JSON.parse(timelineInfo);
+  for (let i = 0; i < items.length; i++) {
+    if (items[i].date !== date) {
+      continue;
+    }
+    let elements = items[i].elements;
+    for (let j = 0; j < elements.length; j++) {
+      if (elements[j].time !== newTimelineElement.time) {
+        continue;
+      }
+      elements[j].body = newTimelineElement.body;
+      return items;
+    }
+    items[i].elements.push(newTimelineElement);
+    return items;
+  }
+  items.push({ 'date': date, elements: [newTimelineElement] });
+  return items;
+}
+
+async function updateDoc(timelineItems) {
+  let newTimelineInfo = JSON.stringify(timelineItems);
+  let docRef = db.collection("contacts").doc(currentContactId);
+  try {
+    await docRef.update({ 'Timeline': newTimelineInfo });
+    console.log("Document successfully written!");
+    showTimeline(currentContactId);
+  } catch (error) {
+    console.error("Error adding document: ", error);
+  }
+}
+
+function editTimelineItem(event) {
+  let editButton = event.target;
+  editButton.style = 'display: none;';
+  let bodyDiv = editButton.previousSibling;
+  bodyDiv.style = 'display: none;';
+  let bodyHtml = bodyDiv.innerHTML;
+  let bodyMarkdown = bodyDiv.getAttribute('data-markdown');
+  let editInput = document.createElement('textarea');
+  editInput.id = 'editInput';
+  editInput.value = bodyMarkdown;
+  insertAfter(editInput, editButton);
+  editInput.focus();
+  let saveButton = document.createElement('button');
+  saveButton.id = 'saveChanges';
+  saveButton.innerHTML = 'Save';
+  saveButton.addEventListener('click', saveChanges);
+  insertAfter(saveButton, editInput);
+  let cancelButton = document.createElement('button');
+  cancelButton.id = 'discardChanges';
+  cancelButton.innerHTML = 'Cancel';
+  cancelButton.addEventListener('click', discardChanges);
+  insertAfter(cancelButton, saveButton);
+}
+
+async function saveChanges(event) {
+  let saveButton = event.target;
+  let markdown = saveButton.previousSibling.value;
+  let time = saveButton.previousSibling.previousSibling.previousSibling.previousSibling.innerHTML;
+  let date = saveButton.parentNode.id;
+  let newTimelineElement = { "time": time, "body": markdown };
+  let docRef = db.collection("contacts").doc(currentContactId);
+  try {
+    let doc = await docRef.get();
+    if (doc.exists) {
+      let items = addItemToDoc(doc, newTimelineElement, date);
+      await updateDoc(items);
+    } else {
+      // doc.data() will be undefined in this case
+      console.log(`No document with ID ${id}`);
+    }
+  } catch (error) {
+    console.log(`Error getting document with ID ${id}:`, error);
+  }
+}
+
+function discardChanges(event) {
+  let cancelButton = event.target;
+  removeEditControls(cancelButton);
+}
+
+function removeEditControls(cancelButton) {
+  let bodyDiv = cancelButton.previousSibling.previousSibling.previousSibling.previousSibling;
+  bodyDiv.style = 'display: block;';
+  let editButton = cancelButton.previousSibling.previousSibling.previousSibling;
+  cancelButton.previousSibling.previousSibling.remove();
+  cancelButton.previousSibling.remove();
+  cancelButton.remove();
+  editButton.style = 'display: block;';
+  editButton.focus();
+}
+
+function insertAfter(newNode, referenceNode) {
+  referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}
+
+function getCurrentTime() {
+  let date = new Date();
+  return `${date.getHours()}:${date.getMinutes()}`;
+}
+
 function showContactFormForEdit(doc) {
   showNewContactForm();
+  document.getElementById('contactFormHeading').innerHTML = `Edit contact card for ${doc.get('Name')}`;
   hideDiv('new_contact_buttons');
   showDiv('update_contact_buttons');
   addListenerToForm(updateContact);
@@ -310,7 +539,7 @@ async function updateContact(event) {
   }
   let doc = getDocFromForm();
   try {
-    await db.collection("contacts").doc(currentIdUnderEditing).set(doc);
+    await db.collection("contacts").doc(currentContactId).update(doc);
     console.log("Document successfully written!");
     window.location.reload();
   } catch (error) {
@@ -321,7 +550,6 @@ async function updateContact(event) {
   render(state);
   return true;
 }
-
 
 function addListenerToForm(listner) {
   let form = document.getElementsByTagName('form')[0];
@@ -343,7 +571,7 @@ async function removeContact(event) {
     return false;
   }
   try {
-    await db.collection("contacts").doc(currentIdUnderEditing).delete();
+    await db.collection("contacts").doc(    currentContactId).delete();
     console.log("Document successfully deleted!");
     window.location.reload();
   } catch (error) {
@@ -359,8 +587,9 @@ async function backup() {
   let data = await querySnapshot.docs.map(doc => doc.data());
   var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(data, null, 2));
   var downloadAnchorNode = document.createElement('a');
-  downloadAnchorNode.setAttribute("href",     dataStr);
-  downloadAnchorNode.setAttribute("download", "backup.json");
+  downloadAnchorNode.setAttribute("href", dataStr);
+  let currentDate = new Date().toDateString();
+  downloadAnchorNode.setAttribute("download", `backup - ${currentDate}.json`);
   document.body.appendChild(downloadAnchorNode); // required for firefox
   downloadAnchorNode.click();
   downloadAnchorNode.remove();
@@ -373,7 +602,7 @@ const productionDomains = [
 ]
 
 function isOnProd() {
-  if (location.hostname in productionDomains) {
+  if (productionDomains.includes(location.hostname)) {
     return true;
   } else {
     return false;
