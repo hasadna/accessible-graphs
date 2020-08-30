5b0a4ea9576a69c6d6709756854656f7b16f1af9
mohammad-suliman <mohmad.s93@gmail.com>
2020-08-30 12:59:30 +0300

    Custom info when space is pressed (#196)

--- ts/view/view.ts
+++ ts/view/view.ts
@@ -401,7 +401,14 @@ function getTextToReportOnSpace() {
   let minValue: string = Math.min(...data).toFixed(2);
   let maxValue: string = Math.max(...data).toFixed(2);
   let average: string = getAverage(data).toFixed(2);
-  let textToReport = `Position ${currentPosition} out of ${graphValuesNum}. Maximum value is ${maxValue}, minimum is ${minValue}, average is ${average}`;
+  let textToReport: string = getUrlParam('spaceInfo');
+  if (textToReport !== '') {
+    textToReport = textToReport.replace('$MIN$', minValue);
+    textToReport = textToReport.replace('$MAX$', maxValue);
+    textToReport = textToReport.replace('$AVERAGE$', average);
+    return textToReport;
+  }
+  textToReport = `Position ${currentPosition} out of ${graphValuesNum}. Maximum value is ${maxValue}, minimum is ${minValue}, average is ${average}`;
   let xValue = data[focusedColIndex];
   let min: number = Math.min(...data);
   let max: number = Math.max(...data);
